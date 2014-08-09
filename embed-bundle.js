6f7ac74f283a07f89548fe0174e350872ee9e350
Max Ogden <max@maxogden.com>
2014-08-09 00:50:17 -0700

    deploy

--- embed-bundle.js
+++ embed-bundle.js
@@ -655,36 +655,7 @@ function parseHost(host) {
   return out;
 }
 
-},{"querystring":5}],2:[function(require,module,exports){
-var nets = require('nets')
-
-module.exports = getGistFiles
-
-function getGistFiles(gist, keys, callback) {
-  var files = gist.data.files
-  
-  var truncated = keys.filter(function(name) {
-    return files[name] && files[name].truncated
-  })
-  
-  if (truncated.length === 0) return callback(err)
-  
-  getNext()
-  
-  function getNext() {
-    var file = truncated.pop()
-    if (!file) return callback(null, gist)
-    nets({url: files[file].raw_url}, done)
-
-    function done(err, resp, content) {
-      if (err) return callback(err)
-      files[file].content = content
-      setTimeout(getNext)
-    }
-  }
-}
-
-},{"nets":6}],5:[function(require,module,exports){
+},{"querystring":5}],5:[function(require,module,exports){
 
 /**
  * Object#toString() ref for stringify().
@@ -1003,7 +974,36 @@ function decode(str) {
   }
 }
 
-},{}],4:[function(require,module,exports){
+},{}],2:[function(require,module,exports){
+var nets = require('nets')
+
+module.exports = getGistFiles
+
+function getGistFiles(gist, keys, callback) {
+  var files = gist.data.files
+  
+  var truncated = keys.filter(function(name) {
+    return files[name] && files[name].truncated
+  })
+  
+  if (truncated.length === 0) return callback(null, gist)
+  
+  getNext()
+  
+  function getNext() {
+    var file = truncated.pop()
+    if (!file) return callback(null, gist)
+    nets({url: files[file].raw_url}, done)
+
+    function done(err, resp, content) {
+      if (err) return callback(err)
+      files[file].content = content
+      setTimeout(getNext)
+    }
+  }
+}
+
+},{"nets":6}],4:[function(require,module,exports){
 
 /**
  * Module dependencies
